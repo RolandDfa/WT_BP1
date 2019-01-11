7c5710752293c97fc251630465884ba6845c61e8
RolandDfa <roland.huijskes@planet.nl>
2019-01-11 14:51:59 +0100

    updated dbconnection

--- dbConnection.php
+++ dbConnection.php
@@ -1,4 +1,9 @@
 <?php
-//db connectie hier:
-//onder constructie
+$hostname = "(local)"; 	// naam van server
+$dbname = "FLETNIX";    	// naam van database 
+$username = "sa";      	// gebruikersnaam
+$pw = "Rol_dfa123";      	// password
+$dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname; ConnectionPooling=0", "$username", "$pw");
+$data = $dbh->query("SELECT * FROM WatchHistory ");
+while ($row = $data->fetch()){    echo "$row[movie_id]: $row[customer_mail_address]: $row[watch_date]: $row[price]</br>";}
 ?>
