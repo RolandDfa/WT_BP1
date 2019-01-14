ebb13efb740964149a2807aa5297ab7ea30dbdfc
RolandDfa <roland.huijskes@planet.nl>
2019-01-14 09:20:59 +0100

    edited dbconnection

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,17 @@
 <?php
-$hostname = "(local)"; 	// naam van server
-$dbname = "FLETNIX";    	// naam van database 
-$username = "sa";      	// gebruikersnaam
-$pw = "Rol_dfa123";      	// password
-$dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname; ConnectionPooling=0", "$username", "$pw");
-$data = $dbh->query("SELECT * FROM WatchHistory ");
-while ($row = $data->fetch()){    echo "$row[movie_id]: $row[customer_mail_address]: $row[watch_date]: $row[price]</br>";}
-?>
+$hostname = "(local)";
+$dbname = "themasite";
+$username = "sa";
+$pw = "Rol_dfa123";
+
+
+try
+{
+    $dbh = new PDO("sqlsrv:Server=$hostname;Database=$dbname; ConnectionPooling=0", "$username", "$pw");
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $e) {
+die ( "Fout met de database: {$e->getMessage()} " );
+}
+
